fb98037f0787f08d6dad5004fe0589b3cc4b42be
Akhilapatel25 <144778151+Akhilapatel25@users.noreply.github.com>
2025-08-07 00:21:14 +0530

    Delete SpringBoot2 directory

--- SpringBoot2/src/main/java/question15/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package question15;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import question15.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
