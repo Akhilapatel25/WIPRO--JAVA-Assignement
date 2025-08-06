e38a9435204045e1d1d458f57cc79d8ffadddd1f
Akhilapatel25 <144778151+Akhilapatel25@users.noreply.github.com>
2025-08-06 23:23:02 +0530

    Add files via upload

---
+++ wiproeurekaserver/src/main/java/wiproerukaserver/wiproerukaserver/WiproerukaserverApplication.java
@@ -0,0 +1,13 @@
+package wiproerukaserver.wiproerukaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiproerukaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiproerukaserverApplication.class, args);
+	}
+
+}
