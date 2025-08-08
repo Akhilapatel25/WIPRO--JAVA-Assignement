3e8142cf16875f39d527cf3d8184ee45d671560e
Akhilapatel25 <144778151+Akhilapatel25@users.noreply.github.com>
2025-08-08 23:45:58 +0530

    Add files via upload

--- SpringBoot/src/main/java/question5/RedirectController5.java
+++ SpringBoot/src/main/java/question5/RedirectController5.java
@@ -1,6 +1,6 @@
 package question5;
 
-import org.springframework.stereotype.Controller;
+/*import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,4 +15,27 @@ public class RedirectController5 {
     public String test2() {
         return "test2"; // returns test2.html (Thymeleaf) or test2.jsp
     }
+}*/
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RedirectController5 {
+
+    @GetMapping("/direct/test")
+    public String redirectToTest2() {
+        return "redirect:/example/test2";
+    }
+
+    @GetMapping("/example/test2")
+    @ResponseBody
+    public String test2() {
+        return "✅ Redirected Successfully to /example/test2";
+    }
 }
+
+
+
