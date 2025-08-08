04490878b4c7d14778390ee8874dabd3c327e6c8
Akhilapatel25 <144778151+Akhilapatel25@users.noreply.github.com>
2025-08-08 23:52:23 +0530

    Update question2.java

--- JUNIT/question2.java
+++ JUNIT/question2.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 
 public class question2 {
 
-    // Method to reverse a string
+  
+    
     public static String reverse(String input) {
         if (input == null) {
             throw new IllegalArgumentException("Input cannot be null");
@@ -14,12 +15,14 @@ public class question2 {
         return new StringBuilder(input).reverse().toString();
     }
 
-    // Inner test class
+
+    
     public static class StringUtilTest {
 
         @Test
         public void testReverse_RegularString() {
-            assertEquals("olleh", reverse("hello"));     // OR question2.reverse("hello")
+            assertEquals("olleh", reverse("hello"));   
+            
             assertEquals("avaJ", reverse("Java"));
         }
 
@@ -34,6 +37,7 @@ public class question2 {
         }
     }
 }
+
 
 
 //runs:3 errors:0 failures:0
